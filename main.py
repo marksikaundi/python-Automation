d13ca8c9f805a4fc49fb43d165caf30dc673c429
Mark Sikaundi <sikaundimark@gmail.com>
2021-06-12 08:44:18 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#author:    MARK SIKAUNDI
+#web site: https://marksikaundi.netlify.app
+#please if you like the trick please upster my codes on github
+#first step do install the library, check the command below
+
+pip install pywhatkit
+#if you want to send some massages to whatsapp users if you busy you want to automate them follow these simple steps
+import pywhatkit as py
+py.sendwhatmsg("you put the number you wanna send to", "here you can write a massege you wanna send", 7, 30)
+
+##############################here are comments and these are ignored by the ide##########################
+
+#if you want to watch videos from youtube you can search with python py searching
+import  pywhatkit as kit
+kit.playonyt("Java full course")
+
+#you can use these commands to search using python, just type what you want to search and it will display
+import pywhatkit as kit
+kit.info("whats computer science")
+
+#you have bunch of emails and you want work to be done easly you can use this command to access all your mails.
+import pywhatkit as kit
+kit.mail("infor@mail.com/monday")
+
+#if you love to learn more will give a link
